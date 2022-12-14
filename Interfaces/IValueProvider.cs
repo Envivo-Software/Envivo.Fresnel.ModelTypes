a8f37ef9daef60f7a1504b397deafd25dfd9281b
vijay@envivo <vijay.patel@envivo.co.uk>
2022-12-14 08:29:04 +0000

    feat(Ccnfiguration): Added IValueProvider, for use with DefaultValueAttribute

---
+++ Interfaces/IValueProvider.cs
@@ -0,0 +1,19 @@
+﻿// SPDX-FileCopyrightText: Copyright (c) 2022 Envivo Software
+// SPDX-License-Identifier: Apache-2.0
+namespace Envivo.Fresnel.ModelTypes.Interfaces
+{
+    public interface IValueProvider : IDomainDependency { }
+
+    /// <summary>
+    /// Used to provide default values of the given type
+    /// </summary>
+    public interface IValueProvider<TContext, TResult> : IValueProvider
+    {
+        /// <summary>
+        /// Returns a value using the given object as context
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public TResult GetValue(TContext context);
+    }
+}
