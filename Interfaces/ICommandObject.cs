22353bf12a0c124de7ce34886764e88243bc4996
vijay@envivo <vijay.patel@envivo.co.uk>
2023-08-08 20:12:29 +0100

    feat: Added ICommandObject

---
+++ Interfaces/ICommandObject.cs
@@ -0,0 +1,25 @@
+﻿// SPDX-FileCopyrightText: Copyright (c) 2022-2023 Envivo Software
+// SPDX-License-Identifier: Apache-2.0
+using System;
+
+namespace Envivo.Fresnel.ModelTypes.Interfaces
+{
+    /// <summary>
+    /// Encapsulates a command that requires more intelligence than a standard Method
+    /// </summary>
+    public interface ICommandObject
+    {
+        Guid Id { get; }
+
+        /// <summary>
+        /// Returns TRUE if the command is ready to be executed
+        /// </summary>
+        bool IsReadyToExecute { get; }
+
+        /// <summary>
+        /// Executes the command, and returns an optional result 
+        /// </summary>
+        /// <returns></returns>
+        object Execute();
+    }
+}
