de883b4af1886c64c176ad81412a220093728ab5
vijay@envivo <vijay.patel@envivo.co.uk>
2024-01-28 13:28:02 +0000

    chore: Corrected comments

--- Interfaces/IAggregateLockService.cs
+++ Interfaces/IAggregateLockService.cs
@@ -8,13 +8,13 @@ namespace Envivo.Fresnel.ModelTypes.Interfaces
     public interface IAggregateLockService<TAggregateRoot> : IDomainDependency
     {
         /// <summary>
-        /// Locks the Aggregate Root, and prevents other users from changing it's contents
+        /// Locks the Aggregate Root, and prevents other users from changing its contents while it is locked
         /// </summary>
         /// <param name="aggregateRoot"></param>
         Task<IAggregateLock?> LockAsync(TAggregateRoot aggregateRoot);
 
         /// <summary>
-        /// Unlocks the Aggregate Root, and allows other users to change it's contents
+        /// Unlocks the Aggregate Root, and allows other users to change its contents
         /// </summary>
         /// <param name="aggregateRoot"></param>
         Task UnlockAsync(TAggregateRoot aggregateRoot);
