a87a227d1631e9aa8423c3189f2e51298680491e
vijay@envivo <vijay.patel@envivo.co.uk>
2024-06-24 08:15:48 +0100

    improv(ICommandObject): Added generic overloads for Execute(TContext) : TResult

--- Interfaces/ICommandObject.cs
+++ Interfaces/ICommandObject.cs
@@ -1,13 +1,16 @@
 ﻿// SPDX-FileCopyrightText: Copyright (c) 2022-2024 Envivo Software
 // SPDX-License-Identifier: Apache-2.0
 using System;
+using System.ComponentModel;
 
 namespace Envivo.Fresnel.ModelTypes.Interfaces
 {
     /// <summary>
     /// Encapsulates a command that requires more interaction or behaviour than a standard Method
     /// </summary>
-    public interface ICommandObject
+    [Browsable(false)]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public interface ICommandObjectBase
     {
         Guid Id { get; }
 
@@ -15,11 +18,38 @@ namespace Envivo.Fresnel.ModelTypes.Interfaces
         /// Returns TRUE if the command is ready to be executed
         /// </summary>
         bool IsReadyToExecute { get; }
+    }
 
+    /// <inheritdoc/>
+    public interface ICommandObject : ICommandObjectBase
+    {
         /// <summary>
-        /// Executes the command, and returns an optional result 
+        /// Executes the command
         /// </summary>
         /// <returns></returns>
-        object Execute();
+        void Execute();
+    }
+
+    /// <inheritdoc/>
+    public interface ICommandObject<TContext> : ICommandObjectBase
+        where TContext : class
+    {
+        /// <summary>
+        /// Executes the command using the given parameter
+        /// </summary>
+        /// <returns></returns>
+        void Execute(TContext context);
+    }
+
+    /// <inheritdoc/>
+    public interface ICommandObject<TContext, TResult> : ICommandObjectBase
+        where TContext : class
+        where TResult : class
+    {
+        /// <summary>
+        /// Executes the command using the given parameter, and returns a result 
+        /// </summary>
+        /// <returns></returns>
+        TResult Execute(TContext context);
     }
 }
