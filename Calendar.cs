308809383a608a29afc2e07241c7fa2dc2a4a134
vijay@envivo <vijay.patel@envivo.co.uk>
2025-04-12 17:22:24 +0100

    feat: Added ICalendar, for simple calendar/diary

---
+++ Calendar.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Envivo.Fresnel.ModelTypes.Interfaces;
+
+namespace Envivo.Fresnel.ModelTypes
+{
+    /// <inheritdoc/>
+    public class Calendar : ICalendar
+    {
+        /// <inheritdoc/>
+        public Guid Id { get; set; }
+
+        /// <inheritdoc/>
+        public string Title { get; set; }
+
+        public ICollection<CalendarEntry> Entries { get; set; } = new List<CalendarEntry>();
+
+        /// <inheritdoc/>
+        public IEnumerable<ICalendar.ICalendarEntry> GetEntries(DateTime rangeStart, DateTime rangeEnd)
+        {
+            return
+                Entries
+                .Where(e => e.EntryDate >= rangeStart &&
+                            e.EntryDate <= rangeEnd)
+                .ToList();
+        }
+
+        /// <inheritdoc/>
+        public class CalendarEntry : ICalendar.ICalendarEntry
+        {
+            /// <inheritdoc/>
+            public Guid Id { get; set; }
+
+            /// <inheritdoc/>
+            public DateTime EntryDate { get; set; }
+
+            /// <inheritdoc/>
+            public TimeSpan Duration { get; set; }
+
+            /// <inheritdoc/>
+            public string Title { get; set; }
+
+            /// <inheritdoc/>
+            public string Description { get; set; }
+        }
+    }
+}
---
+++ Interfaces/ICalendar.cs
@@ -0,0 +1,38 @@
+﻿// SPDX-FileCopyrightText: Copyright (c) 2022-2025 Envivo Software
+// SPDX-License-Identifier: Apache-2.0
+using System;
+using System.Collections.Generic;
+using Envivo.Fresnel.ModelTypes.Interfaces;
+
+namespace Envivo.Fresnel.ModelTypes.Interfaces
+{
+    /// <summary>
+    /// A Calendar of entries
+    /// </summary>
+    public interface ICalendar : IEntity
+    {
+        public string Title { get; }
+
+        /// <summary>
+        /// Returns the entries for the given Range
+        /// </summary>
+        /// <param name="rangeStart"></param>
+        /// <param name="rangeEnd"></param>
+        /// <returns></returns>
+        public IEnumerable<ICalendarEntry> GetEntries(DateTime rangeStart, DateTime rangeEnd);
+
+        /// <summary>
+        /// An entry within a calendar
+        /// </summary>
+        public interface ICalendarEntry : IValueObject
+        {
+            public DateTime EntryDate { get; }
+
+            public TimeSpan Duration { get; }
+
+            public string Title { get; }
+
+            public string Description { get; }
+        }
+    }
+}
