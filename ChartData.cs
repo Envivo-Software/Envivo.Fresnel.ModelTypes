1c59c051764b14d6fcdec6c8fb30e8e0b6b72c72
vijay@envivo <vijay.patel@envivo.co.uk>
2025-04-12 17:22:07 +0100

    feat: Added IChartData, for simple graphs & charts

---
+++ ChartData.cs
@@ -0,0 +1,67 @@
+﻿// SPDX-FileCopyrightText: Copyright (c) 2022-2025 Envivo Software
+// SPDX-License-Identifier: Apache-2.0
+using System;
+using Envivo.Fresnel.ModelTypes.Interfaces;
+
+namespace Envivo.Fresnel.ModelTypes
+{
+    /// <inheritdoc cref="IChartData" />
+    public class ChartData : IChartData
+    {
+        // <inheritdoc/>
+        public Guid Id { get; set; }
+
+        // <inheritdoc/>
+        public string ChartType { get; set; }
+
+        // <inheritdoc/>
+        public string Title { get; set; }
+
+        // <inheritdoc/>
+        public string XAxisLabel { get; set; }
+
+        // <inheritdoc/>
+        public string YAxisLabel { get; set; }
+
+        public IChartData.IDataSeries[] Series { get; set; } = Array.Empty<IChartData.IDataSeries>();
+
+
+        /// <inheritdoc cref="IChartData.IDataSeries" />
+        public class DataSeries : IChartData.IDataSeries
+        {
+            // <inheritdoc/>
+            public Guid Id { get; set; }
+
+            // <inheritdoc/>
+            public string SeriesName { get; set; }
+
+            // <inheritdoc/>
+            public string HexColour { get; set; }
+
+            // <inheritdoc/>
+            public IChartData.IDataPoint[] Points { get; set; } = Array.Empty<IChartData.IDataPoint>();
+        }
+
+        /// <inheritdoc cref="IChartData.IDataPoint" />
+        public class DataPoint : IChartData.IDataPoint
+        {
+            // <inheritdoc/>
+            public Guid Id { get; set; }
+
+            // <inheritdoc/>
+            public double XValue { get; set; }
+
+            // <inheritdoc/>
+            public double YValue { get; set; }
+
+            // <inheritdoc/>
+            public double Size { get; set; }
+
+            // <inheritdoc/>
+            public string PointName { get; set; }
+
+            // <inheritdoc/>
+            public string HexColour { get; set; }
+        }
+    }
+}
---
+++ Interfaces/IChartData.cs
@@ -0,0 +1,34 @@
+﻿// SPDX-FileCopyrightText: Copyright (c) 2022-2025 Envivo Software
+// SPDX-License-Identifier: Apache-2.0
+using Envivo.Fresnel.ModelTypes.Interfaces;
+
+namespace Envivo.Fresnel.ModelTypes.Interfaces
+{
+    /// <summary>
+    /// Used for presenting data in a chart
+    /// </summary>
+    public interface IChartData : IEntity
+    {
+        public string ChartType { get; }
+        public string Title { get; }
+        public string XAxisLabel { get; }
+        public string YAxisLabel { get; }
+        public IDataSeries[] Series { get; }
+
+        public interface IDataSeries : IEntity
+        {
+            public string SeriesName { get; }
+            public string HexColour { get; }
+            public IDataPoint[] Points { get; }
+        }
+
+        public interface IDataPoint
+        {
+            public double XValue { get; }
+            public double YValue { get; }
+            public double Size { get; }
+            public string PointName { get; }
+            public string HexColour { get; }
+        }
+    }
+}
