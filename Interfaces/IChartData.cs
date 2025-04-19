2acf4bf059496d61d1d0e16a96731fbc4027762f
vijay@envivo <vijay.patel@envivo.co.uk>
2025-04-19 16:08:47 +0100

    refact: IChartData + IDataSeries no longer treated as IEntity

--- Interfaces/IChartData.cs
+++ Interfaces/IChartData.cs
@@ -1,22 +1,24 @@
 ﻿// SPDX-FileCopyrightText: Copyright (c) 2022-2025 Envivo Software
 // SPDX-License-Identifier: Apache-2.0
-using Envivo.Fresnel.ModelTypes.Interfaces;
+using System;
 
 namespace Envivo.Fresnel.ModelTypes.Interfaces
 {
     /// <summary>
     /// Used for presenting data in a chart
     /// </summary>
-    public interface IChartData : IEntity
+    public interface IChartData
     {
+        public Guid Id { get; }
         public string ChartType { get; }
         public string Title { get; }
         public string XAxisLabel { get; }
         public string YAxisLabel { get; }
         public IDataSeries[] Series { get; }
 
-        public interface IDataSeries : IEntity
+        public interface IDataSeries
         {
+            public Guid Id { get; }
             public string SeriesName { get; }
             public string HexColour { get; }
             public IDataPoint[] Points { get; }
