d6e27a69af5cd749fc6b19c8d7e4b0d2a88e5c21
vijay@envivo <vijay.patel@envivo.co.uk>
2025-10-22 11:51:30 +0100

    feat: ICommandObjectSetup allows Command Objects to be pre-configured from a Domain Object

---
+++ Interfaces/ICommandObjectSetup.cs
@@ -0,0 +1,19 @@
+﻿// SPDX-FileCopyrightText: Copyright (c) 2022-2025 Envivo Software
+// SPDX-License-Identifier: Apache-2.0
+using System.Threading.Tasks;
+
+namespace Envivo.Fresnel.ModelTypes.Interfaces
+{
+    /// <summary>
+    /// Used to configure an ICommandObject with an initial set of values
+    /// </summary>
+    public interface ICommandObjectSetup<TSetupContext>
+        where TSetupContext : class
+    {
+        /// <summary>
+        /// Configures the command from the given object
+        /// </summary>
+        /// <returns></returns>
+        Task SetupAsync(TSetupContext context);
+    }
+}
