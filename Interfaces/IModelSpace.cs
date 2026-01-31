5752d6ca7f39ecf2e514d9652b0c4be4f61c958b
vijay@envivo <vijay.patel@envivo.co.uk>
2026-01-31 15:43:06 +0000

    improv: Added IModelSpace methods to attach objects to the identity map

--- Interfaces/IModelSpace.cs
+++ Interfaces/IModelSpace.cs
@@ -11,6 +11,19 @@ namespace Envivo.Fresnel.ModelTypes.Interfaces
     public interface IModelSpace : IDomainDependency
     {
         /// <summary>
+        /// Ensures the given objects are tracked by the active identity map
+        /// </summary>
+        /// <param name="domainObject"></param>
+        /// <param name="additionalObjects"></param>
+        void AttachObjects(object domainObject, params object[] additionalObjects);
+
+        /// <summary>
+        /// Ensures the given object and it's entire graph are tracked by the active identity map
+        /// </summary>
+        /// <param name="rootObject"></param>
+        void AttachObjectGraph(object rootObject);
+
+        /// <summary>
         /// Returns objects from the active identity map.
         /// If not found, returns the item from a repository.
         /// </summary>
